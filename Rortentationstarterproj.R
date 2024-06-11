3180e8d8a000ed20ae49ef919b306f3d2ea75f32
Ainsley McLaughlin <annerositamc01@gmail.com>
2024-06-11 19:43:25 -0400

    info on how to send to git

--- Rortentationstarterproj.R
+++ Rortentationstarterproj.R
@@ -1,3 +1,15 @@
 #title "Intro R Things"
+
+#How to clone a repo and push things from R to GitHub:
+#Go to github>go to repository>code>copy http link
+#Go to R>New Proj> version control>git>paste copied link, 
+#use same name as in git, and choose a folder in the third line
+#Now have repo on R
+
+#to send to git/send edits:
+#Edit the document then in Upper Right box>Git>Commit>
+#>Check which files>write commit message>Commit>then push bottom to send to git
+
+#Trying this
 x=2
 x
